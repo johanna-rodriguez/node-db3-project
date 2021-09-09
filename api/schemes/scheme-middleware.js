521db538a0c4b67789d6debea140cef6bcb25b06
Johanna Rodriguez <>
2021-09-08 23:48:15 -0500

    fix validateScheme function

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config')
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -9,22 +9,22 @@ const db = require('../../data/db-config')
 */
 const checkSchemeId = async (req, res, next) => {
   try {
-    const existing = await db('schemes')
-      .where('scheme_id', req.params.squeme_id)
-      .first()
+    const existing = await db("schemes")
+      .where("scheme_id", req.params.squeme_id)
+      .first();
 
-      if (!existing) {
-        next({
-          status: 404,
-          message: `scheme with scheme_id ${req.params.squeme_id}not found`,
-        })
-      } else {
-        next()
-      }
+    if (!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.squeme_id}not found`,
+      });
+    } else {
+      next();
+    }
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -35,17 +35,18 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-  const { scheme_name } = req.body
+  const { scheme_name } = req.body;
+
   if (
     scheme_name === undefined ||
-    typeof squeme_name !== 'string' ||
+    typeof scheme_name !== "string" ||
     !scheme_name.trim()
-    ){
-      next ({ status: 400, message: `invalid scheme_name` })
-    } else {
-      next()
-    }
-}
+  ) {
+    next({ status: 400, message: `invalid scheme_name` });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -57,24 +58,24 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-  const { instructions, step_number } = req.body
+  const { instructions, step_number } = req.body;
 
   if (
     instructions === undefined ||
-    typeof instructions !== 'string' ||
+    typeof instructions !== "string" ||
     !instructions.trim() ||
-    typeof step_number !== 'number' ||
+    typeof step_number !== "number" ||
     step_number < 1
-    ) {
-      const error = { status: 400, message: 'invalid step'}
-      next(error)
-    } else {
-      next()
-    }
+  ) {
+    const error = { status: 400, message: "invalid step" };
+    next(error);
+  } else {
+    next();
   }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
