f62e7a1c3383716fb31ae99b6802f934802ad436
Johanna Rodriguez <>
2021-09-09 20:51:59 -0500

    solved multitable queries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,29 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT ProductName, CategoryName
+FROM product;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT Id, ShipName
+FROM Orders
+WHERE OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName, o.Quantity
+FROM Product AS p
+JOIN OrderDetail AS o 
+ON p.Id = o.ProductId
+WHERE o.OrderId = 10251;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.Id, c.CompanyName, e.LastName
+FROM Orders AS o
+JOIN Customer AS c
+ON c.Id = o.CustomerId
+JOIN Employee AS e
+ON e.Id = o.EmployeeId
+
